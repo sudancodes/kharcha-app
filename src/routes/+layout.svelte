7f45bd436e565b34a51dfad84b7d7590bb3e9c4c
Sudan Ghimire <smellsmthgood@gmail.com>
2022-11-26 17:03:32 +0545

    sveltekit and tailwind install

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "../app.css"
+</script>
+<header>
+    <h1 class="text-center text-4xl mt-6">Hi, Sujan</h1>
+    <nav>
+        <a href="/contact">Talk</a>
+    </nav>
+</header>
+<slot></slot>
